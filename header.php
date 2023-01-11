bb7eaabfe43b790c011e09d823603a34dd137ee1
leanecharpentier <leanecharpentierpro@outlook.com>
2023-01-11 09:58:35 +0100

    premier commit

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="icon" type="image/png" href="images/Logo v2.png" />
+    <title>Xmas drop</title>
+</head>
+<body>
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Collections</a></li>
+            <li><a href="index.html"><img class="logo_nav" src="images/Logo v2.png" alt=""></a></li>
+            <li><a href="#">New drop</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+</body>
+</html>
